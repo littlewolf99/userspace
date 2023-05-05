10d3bfbf0f4489945ededcc007c18d64db741382
simonkoener153 <themeparadise06@gmail.com>
2023-05-06 01:38:50 +0900

    Added user info on sidebar

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,5 +1,8 @@
-import { useAuth } from "utils/auth";
 import * as React from "react";
+import { Avatar, Space, Typography } from "antd";
+import { useAuth } from "utils/auth";
+
+const { Text, Paragraph } = Typography;
 
 const UserInfo: React.FC = () => {
   const [user] = useAuth(false);
@@ -8,7 +11,26 @@ const UserInfo: React.FC = () => {
     return null;
   }
 
-  return <div>{user.username}</div>;
+  const initials = (user.firstName[0] + user.lastName[0]).toUpperCase();
+
+  return (
+    <Space direction="vertical" style={{ textAlign: "center" }} size={0}>
+      <Avatar
+        style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}
+        size={50}
+      >
+        {initials}
+      </Avatar>
+
+      <Text strong style={{ display: "block", margin: "10px 0 0" }}>
+        {[user.firstName, user.lastName].join(" ")}
+      </Text>
+
+      <Text type="secondary">@{user.username}</Text>
+
+      <Text type="secondary">{user.email}</Text>
+    </Space>
+  );
 };
 
 export default UserInfo;
