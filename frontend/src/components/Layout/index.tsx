29867588d62067c62d196e13f18289c2ae9e4d5a
simonkoener153 <themeparadise06@gmail.com>
2023-05-08 22:39:48 +0900

    Fix layout to use header component

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
-import { Link, Outlet, useLocation } from "react-router-dom";
-import { Col, Layout, Menu, Row } from "antd";
+import { Outlet, useLocation } from "react-router-dom";
+import { Col, Layout, Menu, Row, notification } from "antd";
 import { ErrorBoundary } from "react-error-boundary";
 import { useAuth } from "utils/auth";
 import Failed from "components/common/Failed";
 import Spinner from "components/common/Spinner";
 import Sidebar from "./Sidebar";
+import Header from "./Header";
 
-const { Header, Content } = Layout;
+const { Content } = Layout;
 
 const contentStyle = {
   padding: "20px 25px",
@@ -23,24 +24,14 @@ const centeredLayoutPaths = ["/signin", "/signup"];
 
 const AppLayout: React.FC = () => {
   useAuth();
+  const [, contextHolder] = notification.useNotification();
   const location = useLocation();
   const isCenteredLayout =
     centeredLayoutPaths.indexOf(location.pathname.trim().toLowerCase()) >= 0;
 
   return (
     <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          selectedKeys={[]}
-          items={[
-            { key: "home", label: <Link to="/">Home</Link> },
-            { key: "signup", label: <Link to="/signup">Signup</Link> },
-          ]}
-        />
-      </Header>
+      <Header />
 
       <Content style={contentStyle}>
         <div style={containerStyle}>
@@ -65,6 +56,8 @@ const AppLayout: React.FC = () => {
           </Row>
         </div>
       </Content>
+
+      {contextHolder}
     </Layout>
   );
 };
